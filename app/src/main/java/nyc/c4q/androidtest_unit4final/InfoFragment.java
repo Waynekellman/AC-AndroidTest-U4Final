c5de2996e25a5f36eb374f29d41849f8ace04a22
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-01-10 21:04:23 -0500

    More button done

--- app/src/main/java/nyc/c4q/androidtest_unit4final/InfoFragment.java
+++ app/src/main/java/nyc/c4q/androidtest_unit4final/InfoFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by justiceo on 1/9/18.
@@ -13,11 +15,23 @@ import android.view.ViewGroup;
 
 public class InfoFragment extends Fragment {
 
+    Button more;
+    TextView textView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.info_fragment, container, false);
+        more = v.findViewById(R.id.more_button);
+        textView = v.findViewById(R.id.more_textView);
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setVisibility(View.VISIBLE);
+                more.setVisibility(View.INVISIBLE);
+            }
+        });
         return v;
     }
 }
