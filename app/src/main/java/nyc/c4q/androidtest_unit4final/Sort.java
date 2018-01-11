61fc4d7b57d397755b026d75b922962f5aa0b8bb
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-01-10 20:51:00 -0500

    some sorting

--- app/src/main/java/nyc/c4q/androidtest_unit4final/Sort.java
+++ app/src/main/java/nyc/c4q/androidtest_unit4final/Sort.java
@@ -22,5 +22,61 @@ public class Sort {
         // You may not use Collections.sort or its equivalent
         // You may not implement another sorting algorithm that is not "selection sort"
         // Tip: Try a version without ordering first.
+        if(isAscending) {
+            for (int i = list.size(); i > 0; i--) {
+                int lastAscendingItem = 0;
+                for (int j = 0; j < i; j++) {
+                    if (list.get(j).compareToIgnoreCase(list.get(lastAscendingItem)) > 0) {
+                        lastAscendingItem = j;
+                    }
+                }
+                String temp = list.get(lastAscendingItem);
+                list.set(lastAscendingItem, list.get(i - 1));
+                list.set(i - 1, temp);
+            }
+        } else {
+            for (int i = 0; i > list.size(); i--) {
+                int lastAscendingItem = list.size() - 1;
+                for (int j = list.size() - 1; j > 0; j--) {
+                    if (list.get(j).compareToIgnoreCase(list.get(lastAscendingItem)) < 0) {
+                        lastAscendingItem = j;
+                    }
+                }
+                String temp = list.get(lastAscendingItem);
+                list.set(lastAscendingItem, list.get(i + 1));
+                list.set(i + 1, temp);
+            }
+        }
+        /**
+         * public void selectionSort(int[] arr) {
+         for (int n = arr.length; n > 0; n--) {      // O(n) - loop
+         // step 1: find largest element
+         int largest = 0;                        // O(1)
+         for(int i = 0; i < n; i++) {            // O(n) inside O(n) => 0(n^2)
+         if (arr[i] > arr[largest]) {        // O(1)
+         largest = i;                    // O(1)
+         }
+         }
+         // step 2: swap the elements
+         int tmp = arr[largest];                 // O(1)
+         arr[largest] = arr[n-1];                // O(1)
+         arr[n-1] = tmp;                         // O(1)
+         }
+         }
+
+         * public static void yokiSort(List<Book> bookList){
+         for (int i = bookList.size(); i > 0; i--) {
+         int lastObject = 0;
+         for (int j = 0; j < i; j++) {
+         if (bookList.get(j).getName().compareTo(bookList.get(lastObject).getName()) > 0) {
+         lastObject = j;
+         }
+         Book temp = bookList.get(lastObject);
+         bookList.set(lastObject, bookList.get(i - 1));
+         bookList.set(i - 1, temp);
+         }
+         }
+         }
+         */
     }
 }
